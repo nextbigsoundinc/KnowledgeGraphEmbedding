8049165fa1b903d6cc86e5b3d3acc18e58d6103b
arembert <aubrey@nextbigsound.com>
2021-07-22 10:39:00 -0400

    moved conve layer to work with kge model

--- codes/model.py
+++ codes/model.py
@@ -18,6 +18,74 @@ from torch.utils.data import DataLoader
 
 from dataloader import TestDataset
 
+from torch.nn.init import xavier_normal_, xavier_uniform_
+
+
+class ConvLayer(nn.Module):
+
+    def __init__(self, entity_embedding, relation_embedding,
+                 input_drop=0.2, hidden_drop=0.3,
+                 feat_drop=0.2,
+                 emb_dim1=20,
+                 hidden_size=9728):
+
+        super(ConvLayer, self).__init__()
+        self.entity_embedding = entity_embedding
+        self.relation_embedding = relation_embedding
+
+        self.inp_drop = torch.nn.Dropout(input_drop)
+        self.hidden_drop = torch.nn.Dropout(hidden_drop)
+        self.feature_map_drop = torch.nn.Dropout2d(feat_drop)
+        # self.loss = torch.nn.BCELoss()  # modify: cosine embedding loss / triplet loss
+        self.emb_dim1 = emb_dim1             # this is from the original configuration in ConvE
+
+        self.nentity = self.entity_embedding.weight.shape[0]
+        self.embedding_dim = self.entity_embedding.weight.shape[1]
+        self.emb_dim2 = self.embedding_dim // self.emb_dim1
+
+        self.conv1 = torch.nn.Conv2d(2, 32, (3, 3), 1, 0, bias=True)
+        self.mpool = nn.MaxPool2d(2, stride=2)
+
+        self.bn0 = torch.nn.BatchNorm2d(2)
+        self.bn1 = torch.nn.BatchNorm2d(32)
+        self.bn2 = torch.nn.BatchNorm1d(self.embedding_dim)
+        self.fc = torch.nn.Linear(hidden_size, self.embedding_dim)
+
+
+    def init(self):
+        xavier_normal_(self.entity_embedding.weight.data)
+        xavier_normal_(self.relation_embedding.weight.data)
+
+
+    def forward(self, e1, rel, batch_size, negative_sample_size):
+
+        e1_embedded = self.entity_embedding(e1).view(batch_size,
+                                                     negative_sample_size,
+                                                     self.emb_dim1,
+                                                     self.emb_dim2)           # len(e1) *  1 * 20 * 10
+        rel_embedded = self.relation_embedding(rel).view(-1, 1, self.emb_dim1, self.emb_dim2)       # len(rel) * 1 * 20 * 10       len(e1) = len(rel)
+
+        stacked_inputs = torch.cat([e1_embedded, rel_embedded], 1)                                  # len * 2 * 20 * 10
+
+        stacked_inputs = self.bn0(stacked_inputs)                   # len * 2 * 20 * 10
+        x = self.inp_drop(stacked_inputs)
+        x = self.conv1(x)                                           # len * 32 * 18 * 8
+
+        x = self.mpool(x)                                           # len * 32 * 9 * 4
+
+        x = self.bn1(x)
+        x = F.relu(x)
+        x = self.feature_map_drop(x)
+
+        x = x.view(x.shape[0], -1)                                  # len * 1152
+        x = self.fc(x)                                              # len * 200
+        x = self.hidden_drop(x)
+        x = self.bn2(x)
+        x = F.relu(x)  # bs * 200
+
+        return x
+
+
 # bash run.sh train RotatE FB15k    0       0      1024        256               1000         24.0    1.0   0.0001 150000         16               -de
 #               1     2      3       4      5        6          7                   8          9       10     11     12           13
 #              mode model  dataset  GPU  saveid    batchsize   neg_sample_size  hidden_dim    gamma   alpha   lr    Max_steps  test_batchsize
@@ -44,20 +112,38 @@ class KGEModel(nn.Module):
         
         self.entity_dim = hidden_dim*2 if double_entity_embedding else hidden_dim
         self.relation_dim = hidden_dim*2 if double_relation_embedding else hidden_dim
-        
-        self.entity_embedding = nn.Parameter(torch.zeros(nentity, self.entity_dim))
-        nn.init.uniform_(
-            tensor=self.entity_embedding, 
-            a=-self.embedding_range.item(), 
-            b=self.embedding_range.item()
-        )
-        
-        self.relation_embedding = nn.Parameter(torch.zeros(nrelation, self.relation_dim))
-        nn.init.uniform_(
-            tensor=self.relation_embedding, 
-            a=-self.embedding_range.item(), 
-            b=self.embedding_range.item()
-        )
+
+        if model_name in ['ConvE', 'CoCoE']:
+            self.entity_embedding = nn.Embedding(self.nentity, self.entity_dim, padding_idx=0)
+            self.relation_embedding = nn.Embedding(self.nrelation, self.relation_dim, padding_idx=0)
+            self.conve_layer_rr = ConvLayer(self.entity_embedding, self.relation_embedding)
+            self.register_parameter('b', nn.Parameter(torch.zeros(self.nentity)))
+
+            if model_name == 'CoCoE':
+                self.img_entity_embedding = nn.Embedding(self.nentity, self.entity_dim, padding_idx=0)
+                self.img_relation_embedding = nn.Embedding(self.nrelation, self.relation_dim, padding_idx=0)
+                self.conv_layer_ri = ConvLayer(self.entity_embedding, self.img_relation_embedding)
+                self.conv_layer_ir = ConvLayer(self.img_entity_embedding, self.relation_embedding)
+                self.conv_layer_ii = ConvLayer(self.img_entity_embedding, self.img_relation_embedding)
+
+        else:
+            self.entity_embedding = nn.Parameter(torch.zeros(nentity, self.entity_dim))
+            nn.init.uniform_(
+                tensor=self.entity_embedding,
+                a=-self.embedding_range.item(),
+                b=self.embedding_range.item()
+            )
+
+            self.relation_embedding = nn.Parameter(torch.zeros(nrelation, self.relation_dim))
+            nn.init.uniform_(
+                tensor=self.relation_embedding,
+                a=-self.embedding_range.item(),
+                b=self.embedding_range.item()
+            )
+
+
+
+
         
         if model_name == 'pRotatE':
             self.modulus = nn.Parameter(torch.Tensor([[0.5 * self.embedding_range.item()]]))
@@ -83,93 +169,142 @@ class KGEModel(nn.Module):
         in their triple ((head, relation) or (relation, tail)).
         '''
 
-        if mode == 'single':
-            batch_size, negative_sample_size = sample.size(0), 1
-            
-            head = torch.index_select(
-                self.entity_embedding, 
-                dim=0, 
-                index=sample[:,0]
-            ).unsqueeze(1)
-            
-            relation = torch.index_select(
-                self.relation_embedding, 
-                dim=0, 
-                index=sample[:,1]
-            ).unsqueeze(1)
-            
-            tail = torch.index_select(
-                self.entity_embedding, 
-                dim=0, 
-                index=sample[:,2]
-            ).unsqueeze(1)
-            
-        elif mode == 'head-batch':
-            tail_part, head_part = sample           # tail part: 1024 * 3 (1024 positive triples)
-                                                    # head part: 1024 * 256 (each row represent neg sample ids of the corresponding positive triple)
-                                                    # in other words, each positive triplet have 256 negetive triplets
-            batch_size, negative_sample_size = head_part.size(0), head_part.size(1)     # 1024 256
-            
-            head = torch.index_select(
-                self.entity_embedding, 
-                dim=0, 
-                index=head_part.view(-1)
-            ).view(batch_size, negative_sample_size, -1)                # indexes * entity_dim: (1024 * 256) * entity_dim
-                                                                        # corrupted head
-            
-            relation = torch.index_select(
-                self.relation_embedding, 
-                dim=0, 
-                index=tail_part[:, 1]
-            ).unsqueeze(1)                                              # 1024 * 1 * entity_dim
-            
-            tail = torch.index_select(
-                self.entity_embedding, 
-                dim=0, 
-                index=tail_part[:, 2]
-            ).unsqueeze(1)                                              # 1024 * 1 * entity_dim
-            
-        elif mode == 'tail-batch':
-            head_part, tail_part = sample
-            batch_size, negative_sample_size = tail_part.size(0), tail_part.size(1)
-            
-            head = torch.index_select(
-                self.entity_embedding, 
-                dim=0, 
-                index=head_part[:, 0]
-            ).unsqueeze(1)
-            
-            relation = torch.index_select(
-                self.relation_embedding,
-                dim=0,
-                index=head_part[:, 1]
-            ).unsqueeze(1)
-            
-            tail = torch.index_select(
-                self.entity_embedding, 
-                dim=0, 
-                index=tail_part.view(-1)
-            ).view(batch_size, negative_sample_size, -1)
-            
+        batch_size=0, negative_sample_size=0
+
+        if self.model_name not in ['TransE', 'DistMult', 'ComplEx', 'RotatE', 'pRotatE', 'ComplEx']:
+            batch_size = sample.size(0)
+
+            if mode == 'single':
+                batch_size, negative_sample_size = sample.size(0), 1
+                head = sample[:, 0]
+                relation = sample[:, 1]
+                tail = sample[:, 2]
+
+            elif mode == 'head-batch':
+                tail_part, head_part = sample  # tail part: 1024 * 3 (1024 positive triples)
+                # head part: 1024 * 256 (each row represent neg sample ids of the corresponding positive triple)
+                # in other words, each positive triplet have 256 negetive triplets
+                batch_size, negative_sample_size = head_part.size(0), head_part.size(1)  # 1024 256
+                head = head_part.view(-1)
+                relation = tail_part[:, 1]
+                tail = tail_part[:, 2]
+
+            elif mode == 'tail-batch':
+                head_part, tail_part = sample
+                batch_size, negative_sample_size = tail_part.size(0), tail_part.size(1)
+                head = head_part[:, 0]
+                relation = head_part[:, 1]
+                tail = tail_part.view(-1)
+
+            else:
+                raise ValueError('mode %s not supported' % mode)
+
         else:
-            raise ValueError('mode %s not supported' % mode)
+
+            if mode == 'single':
+                batch_size, negative_sample_size = sample.size(0), 1
+
+                head = torch.index_select(
+                    self.entity_embedding,
+                    dim=0,
+                    index=sample[:,0]
+                ).unsqueeze(1)
+
+                relation = torch.index_select(
+                    self.relation_embedding,
+                    dim=0,
+                    index=sample[:,1]
+                ).unsqueeze(1)
+
+                tail = torch.index_select(
+                    self.entity_embedding,
+                    dim=0,
+                    index=sample[:,2]
+                ).unsqueeze(1)
+
+            elif mode == 'head-batch':
+                tail_part, head_part = sample           # tail part: 1024 * 3 (1024 positive triples)
+                                                        # head part: 1024 * 256 (each row represent neg sample ids of the corresponding positive triple)
+                                                        # in other words, each positive triplet have 256 negetive triplets
+                batch_size, negative_sample_size = head_part.size(0), head_part.size(1)     # 1024 256
+
+                head = torch.index_select(
+                    self.entity_embedding,
+                    dim=0,
+                    index=head_part.view(-1)
+                ).view(batch_size, negative_sample_size, -1)                # indexes * entity_dim: (1024 * 256) * entity_dim
+                                                                            # corrupted head
+
+                relation = torch.index_select(
+                    self.relation_embedding,
+                    dim=0,
+                    index=tail_part[:, 1]
+                ).unsqueeze(1)                                              # 1024 * 1 * entity_dim
+
+                tail = torch.index_select(
+                    self.entity_embedding,
+                    dim=0,
+                    index=tail_part[:, 2]
+                ).unsqueeze(1)                                              # 1024 * 1 * entity_dim
+
+            elif mode == 'tail-batch':
+                head_part, tail_part = sample
+                batch_size, negative_sample_size = tail_part.size(0), tail_part.size(1)
+
+                head = torch.index_select(
+                    self.entity_embedding,
+                    dim=0,
+                    index=head_part[:, 0]
+                ).unsqueeze(1)
+
+                relation = torch.index_select(
+                    self.relation_embedding,
+                    dim=0,
+                    index=head_part[:, 1]
+                ).unsqueeze(1)
+
+                tail = torch.index_select(
+                    self.entity_embedding,
+                    dim=0,
+                    index=tail_part.view(-1)
+                ).view(batch_size, negative_sample_size, -1)
+
+            else:
+                raise ValueError('mode %s not supported' % mode)
             
         model_func = {
             'TransE': self.TransE,
             'DistMult': self.DistMult,
             'ComplEx': self.ComplEx,
             'RotatE': self.RotatE,
-            'pRotatE': self.pRotatE
+            'pRotatE': self.pRotatE,
+            'ConvE': self.ConvE,
+            'CoCoE': self.CoCoE
         }
         
         if self.model_name in model_func:
-            score = model_func[self.model_name](head, relation, tail, mode)
+            score = model_func[self.model_name](head, relation, tail, mode, batch_size, negative_sample_size)
         else:
             raise ValueError('model %s not supported' % self.model_name)
         
         return score
-    
-    def TransE(self, head, relation, tail, mode):
+
+    def ConvE(self, head, relation, tail, mode, batch_size=0, negative_sample_size=0):
+
+        if mode=='head_batch':
+            x = self.conve_layer_rr(head, relation, batch_size, negative_sample_size)
+            x = torch.mm(x, self.entity_embedding(tail).weight.transpose(1, 0))  # len * 200  @ (200 * # ent)  => len *  # ent
+            x += self.b.expand_as(x)
+            score = torch.sigmoid(x)
+        else:
+            x = self.conve_layer_rr(head, relation, -1, 1)
+            x = torch.mm(x, self.entity_embedding(tail).weight.transpose(1, 0))  # len * 200  @ (200 * # ent)  => len *  # ent
+            x += self.b.expand_as(x)
+            score = torch.sigmoid(x)
+
+        return score  # len * # ent
+
+    def TransE(self, head, relation, tail, mode, batch_size=0, negative_sample_size=0):
         if mode == 'head-batch':
             score = head + (relation - tail)
         else:
@@ -178,7 +313,7 @@ class KGEModel(nn.Module):
         score = self.gamma.item() - torch.norm(score, p=1, dim=2)
         return score
 
-    def DistMult(self, head, relation, tail, mode):
+    def DistMult(self, head, relation, tail, mode, batch_size=0, negative_sample_size=0):
         if mode == 'head-batch':
             score = head * (relation * tail)
         else:
@@ -190,7 +325,7 @@ class KGEModel(nn.Module):
     # def ConvE(self, head, relation, tail, mode):
 
 
-    def ComplEx(self, head, relation, tail, mode):
+    def ComplEx(self, head, relation, tail, mode, batch_size=0, negative_sample_size=0):
         re_head, im_head = torch.chunk(head, 2, dim=2)
         re_relation, im_relation = torch.chunk(relation, 2, dim=2)
         re_tail, im_tail = torch.chunk(tail, 2, dim=2)
@@ -207,7 +342,7 @@ class KGEModel(nn.Module):
         score = score.sum(dim = 2)
         return score
 
-    def RotatE(self, head, relation, tail, mode):
+    def RotatE(self, head, relation, tail, mode, batch_size=0, negative_sample_size=0):
         # head (if corrupted): 1024 * 256 * ent_dim         (ent_dim = hidden_dim * 2)
         # relation: 1024 * 1 * hidden_dim
         # tail: 1024 * 1 * ent_dim
@@ -242,7 +377,7 @@ class KGEModel(nn.Module):
         score = self.gamma.item() - score.sum(dim = 2)              # 1024 * 256
         return score
 
-    def pRotatE(self, head, relation, tail, mode):
+    def pRotatE(self, head, relation, tail, mode, batch_size=0, negative_sample_size=0):
         pi = 3.14159262358979323846
         
         #Make phases of entities and relations uniformly distributed in [-pi, pi]
