7666244f86b8f903ef217b774257fcfe607e1ca7
arembert <aubrey@nextbigsound.com>
2021-07-23 13:14:03 -0400

    test

--- codes/Model_v2.py
+++ codes/Model_v2.py
@@ -57,6 +57,7 @@ class ConvELayer(nn.Module):
 
     def forward(self, head, rel, tail, batch_size, negative_sample_size):
         print("head embedding=[", head.shape, "]")
+        print("head reshape=[", head.view(batch_size, negative_sample_size, -1).shape, "]")
         print("relationship=[",rel.shape,"]")
         print("tail embedding={", tail.shape, "]")
         tail_samples = tail.view(rel.shape[0], -1).shape[1]
