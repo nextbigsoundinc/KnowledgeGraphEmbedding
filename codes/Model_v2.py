e0fb6c8727bdc63f213a48d01394ae4801c6575b
arembert <aubrey@nextbigsound.com>
2021-07-31 15:02:15 -0400

    revert

--- codes/Model_v2.py
+++ codes/Model_v2.py
@@ -75,7 +75,6 @@ class ComplExDeep(nn.Module):
 
     def __init__(self,
                  embedding_dim,
-                 channel_size=256,
                  hidden_size=256,
                  input_drop=0.5,
                  hidden_drop=0.5
@@ -352,7 +351,7 @@ class KGEModel(nn.Module):
             b=self.embedding_range.item()
         )
         if model_name == 'CoCoE':
-            self.cocoe_layer = ComplExDeep(self.entity_dim, channel_size=neg_sample_size, hidden_size=self.hidden_dim)
+            self.cocoe_layer = ComplExDeep(self.entity_dim, hidden_size=self.hidden_dim)
 
         elif model_name == 'ConvE':
             self.conve_layer = ConvELayer(self.entity_dim, self.nentity)
