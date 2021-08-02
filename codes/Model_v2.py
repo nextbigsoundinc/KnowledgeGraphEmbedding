ff16799e6ed05b31762237fe3bfeeab6f6664f21
arembert <aubrey@nextbigsound.com>
2021-08-02 14:22:12 -0400

    revert

--- codes/Model_v2.py
+++ codes/Model_v2.py
@@ -831,6 +831,7 @@ class KGEModel(nn.Module):
                 pred = pred.cuda()
                 target = target.cuda()
             loss = model.loss(pred, target)
+            print("loss=", loss)
             loss.backward()
             log = {
                 'positive_sample_loss': 0,
