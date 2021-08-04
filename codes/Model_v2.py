4aa0dea991880069f11d59c2e9f9c0dad7058224
arembert <aubrey@nextbigsound.com>
2021-08-03 21:00:33 -0400

    test

--- codes/Model_v2.py
+++ codes/Model_v2.py
@@ -127,9 +127,8 @@ class RotatEDeep(nn.Module):
         self.input_neurons = int(input_neurons * 0.5)
         self.hidden_drop = torch.nn.Dropout(0.5)
         self.input_drop = torch.nn.Dropout(0.5)
-        self.fc1 = torch.nn.Linear(self.input_neurons, 512)
-        self.fc2 = torch.nn.Linear(512, 256)
-        self.fc3 = torch.nn.Linear(256, 64)
+        self.fc1 = torch.nn.Linear(self.input_neurons, 256)
+        self.fc2 = torch.nn.Linear(256, 1)
 
     def forward(self, head, relation, tail, mode):
 
@@ -163,11 +162,9 @@ class RotatEDeep(nn.Module):
 
         x = F.relu(self.hidden_drop(self.fc1(score)))
         x = self.fc2(x)
-        score = self.fc3(x)
-        score = score.sum(dim=2)
+        score = x.sum(dim=2)
         # print('score1.shape=', score1.shape)
         return score
-
 
 
 class ConvELayer(nn.Module):
