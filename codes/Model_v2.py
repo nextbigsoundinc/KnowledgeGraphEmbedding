3cd63c282fe22899c8f0cf34c49234a02a0fe81c
arembert <aubrey@nextbigsound.com>
2021-08-11 11:57:34 -0400

    adjust loss

--- codes/Model_v2.py
+++ codes/Model_v2.py
@@ -706,6 +706,41 @@ class KGEModel(nn.Module):
             true_dist.scatter_(1, indices, confidence)
         return true_dist
 
+    @staticmethod
+    def unified_negative_sampling_loss(args, positive_score, negative_score, model, subsampling_weight):
+        if args.negative_adversarial_sampling:
+            # In self-adversarial sampling, we do not apply back-propagation on the sampling weight
+            negative_score = (
+                    F.softmax(negative_score * args.adversarial_temperature, dim=1).detach()
+                    * F.logsigmoid(-negative_score)).sum(dim=1)
+        else:
+            negative_score = F.logsigmoid(-negative_score).mean(dim=1)
+
+        positive_score = F.logsigmoid(positive_score).squeeze(dim=1)
+
+        if args.uni_weight:
+            positive_sample_loss = - positive_score.mean()
+            negative_sample_loss = - negative_score.mean()
+        else:
+            positive_sample_loss = - (
+                    subsampling_weight * positive_score).sum() / subsampling_weight.sum()
+            negative_sample_loss = - (
+                    subsampling_weight * negative_score).sum() / subsampling_weight.sum()
+
+        loss = (positive_sample_loss + negative_sample_loss) / 2
+
+        if args.regularization != 0.0:
+            # Use L3 regularization for ComplEx and DistMult
+            regularization = args.regularization * (
+                    model.entity_embedding.norm(p=3) ** 3 +
+                    model.relation_embedding.norm(p=3).norm(p=3) ** 3
+            )
+            loss = loss + regularization
+            regularization_log = {'regularization': regularization.item()}
+        else:
+            regularization_log = {}
+
+        return loss, regularization_log, positive_sample_loss, negative_sample_loss
 
     @staticmethod
     def train_step(model, optimizer, train_iterator, args):
@@ -727,38 +762,9 @@ class KGEModel(nn.Module):
 
         if model.model_name not in ['RotatEDeep', 'ComplExDeep', 'ConvE', 'CoCoE']:
 
-            if args.negative_adversarial_sampling:
-                # In self-adversarial sampling, we do not apply back-propagation on the sampling weight
-                negative_score = (
-                        F.softmax(negative_score * args.adversarial_temperature, dim=1).detach()
-                        * F.logsigmoid(-negative_score)).sum(dim=1)
-            else:
-                negative_score = F.logsigmoid(-negative_score).mean(dim=1)
-
-            positive_score = F.logsigmoid(positive_score).squeeze(dim=1)
-
-            if args.uni_weight:
-                positive_sample_loss = - positive_score.mean()
-                negative_sample_loss = - negative_score.mean()
-            else:
-                positive_sample_loss = - (
-                            subsampling_weight * positive_score).sum() / subsampling_weight.sum()
-                negative_sample_loss = - (
-                            subsampling_weight * negative_score).sum() / subsampling_weight.sum()
-
-            loss = (positive_sample_loss + negative_sample_loss) / 2
-
-            if args.regularization != 0.0:
-                # Use L3 regularization for ComplEx and DistMult
-                regularization = args.regularization * (
-                        model.entity_embedding.norm(p=3) ** 3 +
-                        model.relation_embedding.norm(p=3).norm(p=3) ** 3
-                )
-                loss = loss + regularization
-                regularization_log = {'regularization': regularization.item()}
-            else:
-                regularization_log = {}
-
+            loss, regularization_log, positive_sample_loss, negative_sample_loss = \
+                KGEModel.unified_negative_sampling_loss(
+                    args, positive_score, negative_score, model, subsampling_weight)
             loss.backward()
 
             log = {
@@ -780,7 +786,13 @@ class KGEModel(nn.Module):
             if args.cuda:
                 pred = pred.cuda()
                 target = target.cuda()
-            loss = model.loss(pred, target)
+            bce_loss = model.loss(pred, target)
+
+            uns_loss, regularization_log, positive_sample_loss, negative_sample_loss = \
+                KGEModel.unified_negative_sampling_loss(
+                    args, positive_score, negative_score, model, subsampling_weight)
+
+            loss = 0.8*bce_loss + 0.2*uns_loss
 
             loss.backward()
 
