34846e3f0b5f727c8fb39b2379c17f0575a17283
arembert <aubrey@nextbigsound.com>
2021-08-16 16:02:22 -0400

    smooth cross entropy loss

--- codes/Model_v2.py
+++ codes/Model_v2.py
@@ -726,7 +726,7 @@ class KGEModel(nn.Module):
         negative_score = model((positive_sample, negative_sample), mode=mode)
         positive_score = model(positive_sample)
 
-        if model.model_name not in ['RotatEDeep', 'ConvE', 'CoCoE']:
+        if model.model_name not in ['ComplExDeep', 'RotatEDeep', 'ConvE', 'CoCoE']:
             if args.negative_adversarial_sampling:
                 # In self-adversarial sampling, we do not apply back-propagation on the sampling weight
                 negative_score = (
